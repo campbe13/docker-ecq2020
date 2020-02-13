88d98fb84d893e29d7e8e9ce678d237576a454dc
Patricia Campbell <x@y.ca>
2020-02-13 15:00:23 -0500

    YAY, fixed flask, default binds to localhost, changed to host-0.0.0.0

--- flaskpy/hello.py
+++ flaskpy/hello.py
@@ -2,11 +2,12 @@
 # pmc sample flask app
 from flask import Flask
 app = Flask(__name__)
-
 @app.route("/")
 def hello():
-   print ("FLASK contacted")
-   return "HTTP 1.1 200 \n\n<body><h2>Hello World!</h2> <h3> Running Flask from Docker</h3></body>"
+
+   return "<body><h2>Hello World!</h2> <h3> Running Flask from Docker</h3></body>"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+#    default binds to localhost
+#    app.run(debug=True)
+    app.run(host="0.0.0.0",debug=True)
