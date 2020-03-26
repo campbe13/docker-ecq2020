2a17050f8592a2356ffe6a3a094f7ec886ec11b4
P Campbell <pcampbell.edu@gmail.com>
2020-03-25 23:17:52 -0400

    HelloWorld added

---
+++ simple-java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * This is the Hello World Example 
+ * @author Sybil N. Gineer
+ * @version 1
+ */
+
+public class HelloWorld  {
+   public static void main(String[] args)    {
+   
+      System.out.println("Hello World!  from Java ");
+   }
+}
