33cae20debfb14bfe49660fb3ede371db54beb0f
P Campbell <pcampbell.edu@gmail.com>
2021-05-07 15:40:31 -0400

    Update standalone-mysql.sh

--- mysql-example/standalone-mysql.sh
+++ mysql-example/standalone-mysql.sh
@@ -2,8 +2,8 @@
 # 2020-03-10
 # PMCampbell
 # run a mysql container db (or a couple of dbs) 
+# change rootpassword if you wish
 
-#if [[ $1 == "-h"]] || [[ $1 == "--help" ]] || [[ $1 == "help" ]] ; then
 if [[ $1 == "-h" ||  $1 == "--help"  ||  $1 == "help" ]] ; then
       echo $(basename $0) this script will launch 2 mysql containers 
       echo $(basename $0) one listens on the standard port of 3306
@@ -11,6 +11,8 @@ if [[ $1 == "-h" ||  $1 == "--help"  ||  $1 == "help" ]] ; then
       echo to access the dbs  mysql -u root -p your.ip.add.ress
       exit 1
 fi
+
+secretpassword=secret-password
 
 # docker run, some options
 # --name   name the image
@@ -20,8 +22,8 @@ fi
 # mysql:latest    the base image from docker hub
 
 # first instance of db
-docker run --name mysql-container1 -d -e MYSQL_ROOT_PASSWORD=secret-password -p 3306:3306 mysql:latest
+docker run --name mysql-container1 -d -e MYSQL_ROOT_PASSWORD=$secretpassword -p 3306:3306 mysql:latest
 
 # second instance of db note port 3307, cannot bind to same port on host
 # but the db container itself is isolated so 3306 on container
-docker run --name mysql-container2 -d -e MYSQL_ROOT_PASSWORD=secret-password -p 3307:3306 mysql:latest
+docker run --name mysql-container2 -d -e MYSQL_ROOT_PASSWORD=$secretpassword -p 3307:3306 mysql:latest
